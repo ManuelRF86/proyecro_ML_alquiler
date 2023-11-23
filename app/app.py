43cd00db61edf0ef2e9755bc237042985326b758
Manuel_Reina <manuelrf86@hotmail.com>
2023-11-23 18:16:23 +0100

    modificaciones

--- app/app.py
+++ app/app.py
@@ -3,15 +3,17 @@ import pandas as pd
 from PIL import Image
 import streamlit.components.v1 as c
 import pickle
+import os
 
+from funciones import *
 
-with open('modelos_entrenados/modelo_final_GBR.pkl', 'rb') as file:
+directorio_actual = os.getcwd()
+
+with open(os.path.join(directorio_actual, '..', 'models','modelo_final_GBR.pkl'), 'rb') as file:
     modelo_GBR = pickle.load(file)
 
 st.set_page_config(page_title='Alquileres en Sevilla',
                    page_icon='🏠')
-
-df = pd.read_csv('data/df.csv')
 
 df = df.rename(columns={'latitude':'lat', 'longitude':'lon'})
 
@@ -19,7 +21,7 @@ seleccion = st.sidebar.selectbox('Seleccione una opción', ['Introducción','Ten
 
 if seleccion == 'Introducción':
     st.title('¿Cuánto cuesta vivir en Sevilla?')
-    img = Image.open('img/img_1.jpg')
+    img = Image.open(os.path.join(directorio_actual, '..', 'img','img_1.jpg'))
     st.image(img)
     with st.expander('Introducción'):
         st.write('Esta aplicación te permitirá, obtener una recomendación del precio de alquiler de una vivienda situada en el área metropolitana de Sevilla')
@@ -30,7 +32,7 @@ if seleccion == 'Introducción':
 
 elif seleccion == 'Tengo una vivienda y la quiero alquilar':
     st.title('Calcular el precio de alquiler de tu vivienda')
-    img_2 = Image.open('img/img_2.jpg')
+    img_2 = Image.open(os.path.join(directorio_actual, '..', 'img','img_2.jpg'))
     st.image(img_2)
 
     e_poblacion_distrito = st.selectbox('Elige un población/distrito:', sorted(df['poblacion / distrito'].unique()))
@@ -60,40 +62,13 @@ elif seleccion == 'Tengo una vivienda y la quiero alquilar':
 
     e_tipo_vivienda = st.selectbox('Elige el tipo de vivienda:', ['Piso','Estudio','Adosado','Duplex','Ático','Casa pareada','Chalet independiente'])
     
-
-    def calcular_alquiler(tamaño,poblacion_distrito,parking,tipo_vivienda,habitaciones,baños):
-    
-        dicc_tipo = {'Estudio' : 1,'Piso' : 2,'Adosado' : 3,'Duplex' : 4,'Ático' : 5,'Casa pareada' : 6,'Chalet independiente' : 7}
-        dicc = {}
-        
-        dicc['size'] = tamaño
-
-        for i, pob_dis in enumerate(df['poblacion / distrito']):
-            if pob_dis == poblacion_distrito:
-                for n, codigo_dist in enumerate(df['codigo_distrito']):
-                    if n == i:
-                        dicc['codigo_distrito'] = codigo_dist
-                        break
-        if parking == 'No':            
-            dicc['parking'] = 0
-        else:
-            dicc['parking'] = 1
-
-        for tipo,codigo in dicc_tipo.items():
-            if tipo == tipo_vivienda:
-                dicc['codigo_tipo'] = codigo
+    e_piscina = st.selectbox('Elije si la vivienda dispone de piscina:', ['Sí','No'])
 
         
-        dicc['total_rooms'] = habitaciones + baños
-    
-    
-        return(dicc)
-    
-    
     calcular = st.sidebar.button('Calcular')
     seccion_contenido = st.empty()
     if calcular:
-        vivienda = pd.DataFrame([calcular_alquiler(e_tamaño, e_poblacion_distrito, e_parking, e_tipo_vivienda, e_habitaciones, e_baños)], index=['mi_indice'])
+        vivienda = pd.DataFrame([calcular_alquiler(e_tamaño, e_poblacion_distrito, e_parking, e_tipo_vivienda,e_piscina, e_habitaciones, e_baños)], index=['mi_indice'])
         prediction = int(modelo_GBR.predict(vivienda))
 
         st.title(f"El precio de alquiler de tu vivienda es: {prediction}€")
